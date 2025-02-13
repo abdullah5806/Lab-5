0001730e81a94824929f38aa9d782e1c1126f7fd
abdullah5806 <abdullahimtiaz5806@gmail.com>
2025-02-13 13:49:42 +0500

    Update copy.js

--- copy.js
+++ copy.js
@@ -5,3 +5,11 @@ copyFileSync("node_modules/jquery/dist/jquery.min.js", "web/jquery.min.js");
 copyFileSync("node_modules/jquery.terminal/js/jquery.terminal.min.js", "web/jquery.terminal.min.js");
 copyFileSync("node_modules/jquery.terminal/js/jquery.mousewheel-min.js", "web/jquery.mousewheel-min.js");
 copyFileSync("node_modules/jquery.terminal/css/jquery.terminal.min.css", "web/terminal.css");
+
+Console.WriteLine("Name: Muhammad Abdullah ");
+Console.WriteLine("Roll No.: 24-ST-020 ");
+
+int a=20 , b=30 ;
+int sum ;
+sum = a + b ;
+Console.WriteLine($" The Sum of Two Numbers is {sum} ");
